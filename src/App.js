1e0d890b5f99c40041e41170b98adaf8d897f9c7
kinghennry <keelechiogbu@gmail.com>
2022-01-07 11:12:10 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Header,
+  WhatGPT3,
+  Footer,
+  Blog,
+  Features,
+  Possibility,
+} from "./container";
+import "./App.css";
+import { Navbar, Brand, CTA } from "./components";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
